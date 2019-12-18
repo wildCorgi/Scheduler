5e3bd04a7fb455a78b3a402d18540da1a53d7625
kamelMohsen <kamelmohsenkamel@gmail.com>
2019-12-18 12:30:01 +0200

    added

--- OS_Scheduler-20191129T081240Z-001/OS_Scheduler/code/process.c
+++ OS_Scheduler-20191129T081240Z-001/OS_Scheduler/code/process.c
@@ -3,6 +3,7 @@
 
 /* Modify this file as needed*/
 clock_t remainingtime;
+
 int main(int agrc, char * argv[])
 {
 
@@ -10,26 +11,17 @@ int main(int agrc, char * argv[])
     clock_t current_time,start_t,last;
     current_time = 0;
     remainingtime=(clock_t)atoi(argv[1]);
-
     last=0;
-    
+
     while (remainingtime > current_time)
     {  
-          
-  
         start_t = clock();
         current_time = (double)((start_t)/(CLOCKS_PER_SEC)) ;
         // if(last!=current_time)
         // {
         //     last=current_time;
-
-        // }
-        
+        // }    
     }
-
-   
     kill(getppid(),SIGUSR1);
     exit(0);
-    
-    
 }
